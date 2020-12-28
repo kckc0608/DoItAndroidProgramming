6da8793f2f063272516022d04efcded9ae82e66c
Neang_Chi <64959010+kckc0608@users.noreply.github.com>
2020-12-28 18:36:53 +0900

    Ex8

--- Ex8_TouchEvent/app/src/main/java/com/example/ex8_touchevent/MainActivity.java
+++ Ex8_TouchEvent/app/src/main/java/com/example/ex8_touchevent/MainActivity.java
@@ -1,14 +1,106 @@
 package com.example.ex8_touchevent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.sql.DriverManager.println;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    GestureDetector detector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        View view = findViewById(R.id.view);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+
+                float curX = event.getX();
+                float curY = event.getY();
+
+                if (action == MotionEvent.ACTION_DOWN) {
+                    println("손가락 눌림 : " + curX + ", " + curY);
+                } else if (action == MotionEvent.ACTION_MOVE) {
+                    println("손가락 움직임 : " + curX + ", " + curY);
+                } else if (action == MotionEvent.ACTION_UP) {
+                    println("손가락 뗌 : " + curX + ", " + curY);
+                }
+
+                return true;
+            }
+        });
+
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                println("onDown() called");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+                println("onShowPress() called");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                println("onSingleTapUp() called");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                println("onScroll() called : " + distanceX + ", " + distanceY);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                println("onLongPress() called");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                println("onFling() called : " + velocityX + ", " + velocityY);
+                return true;
+            }
+        });
+
+        View view2 = findViewById(R.id.view2);
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                detector.onTouchEvent(event);
+                return true;
+            }
+        });
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Toast.makeText(this, "시스템 [BACK] 버튼이 눌렸습니다.", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return false;
+    }
+
+    public void println(String data) {
+        textView.append(data+"\n");
+    }
+
 }
