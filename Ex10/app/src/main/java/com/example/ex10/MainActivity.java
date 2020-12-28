24b934b70937ad7c307dbd24d49663b04fefe02c
Neang_Chi <64959010+kckc0608@users.noreply.github.com>
2020-12-29 00:43:42 +0900

    Ex10

--- Ex10/app/src/main/java/com/example/ex10/MainActivity.java
+++ Ex10/app/src/main/java/com/example/ex10/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.ex10;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) { // 가로방향 처리
+            showToast("방향 : ORIENTATION_LANDSCAPE");
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) { // 세로방향 처리
+            showToast("방향 : ORIENTATION_PORTAIT");
+        }
+    }
+
+    public void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
     }
 }
