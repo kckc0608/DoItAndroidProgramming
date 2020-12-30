9caa648d9dd8bdcfa90bcc9a1489d896dcdb0489
Neang_Chi <64959010+kckc0608@users.noreply.github.com>
2020-12-30 22:38:11 +0900

    Update MainActivity.java

--- Ex14/app/src/main/java/com/example/ex14/MainActivity.java
+++ Ex14/app/src/main/java/com/example/ex14/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.ex14;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_SHORT).show();
+            }
+        });
         setContentView(R.layout.activity_main);
     }
 }
