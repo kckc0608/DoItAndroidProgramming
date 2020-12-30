73ffb7441547f1091c2c92f1ef183a2e7bd7fcd5
Neang_Chi <64959010+kckc0608@users.noreply.github.com>
2020-12-30 20:29:01 +0900

    Proj6

--- Proj6/app/src/main/java/com/example/proj6/MainActivity.java
+++ Proj6/app/src/main/java/com/example/proj6/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.proj6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    SeekBar seekBar;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        seekBar = findViewById(R.id.seekBar);
+        editText = findViewById(R.id.editTextTextPersonName);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    progressBar.setProgress(progress);
+                    editText.setText(progress+"");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                progressBar.setProgress(seekBar.getProgress());
+                editText.setText(seekBar.getProgress()+"");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                progressBar.setProgress(seekBar.getProgress());
+                editText.setText(seekBar.getProgress()+"");
+            }
+        });
     }
 }
