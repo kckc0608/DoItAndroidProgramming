34c1681659be2fb9f4c5a3a1c50ce1077ddf935b
Neang_Chi <64959010+kckc0608@users.noreply.github.com>
2021-01-03 02:44:48 +0900

    ex20

--- Ex20/app/src/main/java/com/example/ex20/MainActivity.java
+++ Ex20/app/src/main/java/com/example/ex20/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.ex20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ListFragment.ImageSelectionCallback {
+
+    ListFragment listFragment;
+    ViewerFragment viewerFragment;
+
+    int[] images = {R.drawable.cover1, R.drawable.cover2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        listFragment = (ListFragment) manager.findFragmentById(R.id.listFragment);
+        viewerFragment = (ViewerFragment) manager.findFragmentById(R.id.viewerFragment2);
+    }
+
+    @Override
+    public void onImageSelected(int position) {
+        viewerFragment.setImage(images[position]);
     }
 }
