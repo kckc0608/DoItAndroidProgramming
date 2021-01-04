aca7f17577d04a0f07af391a8c0bee2069756a1d
Neang_Chi <64959010+kckc0608@users.noreply.github.com>
2021-01-04 09:01:38 +0900

    Ex22

--- Ex212/app/src/main/java/com/example/ex21/MainActivity.java
+++ Ex212/app/src/main/java/com/example/ex21/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.ex21;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionBar.setLogo(R.drawable.home);
+                actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME|ActionBar.DISPLAY_USE_LOGO);
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        View v = menu.findItem(R.id.menu_search).getActionView();
+        if (v!= null) {
+            EditText editText = v.findViewById(R.id.textView);
+            if (editText != null) {
+                editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        Toast.makeText(getApplicationContext(), "입력됨.", Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int curId = item.getItemId();
+        switch (curId) {
+            case R.id.menu_refresh:
+                Toast.makeText(this, "refreshed", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_search:
+                //Toast.makeText(this,"search", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_settings:
+                Toast.makeText(this,"settings",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
